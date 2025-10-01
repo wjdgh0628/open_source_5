77d73b59d035c89b498203c36e3157b0f680579c
LazyDev1024 <ironfish1024@gmail.com>
2025-10-01 10:03:25 +0900

    [edit] 기술실증 완료

--- js/main.js
+++ js/main.js
@@ -46,7 +46,7 @@ export function initMap() {
         //건물, 배경 클릭시 실행할 코드 지정
         // map.on("click", "campus-3d", e => handleBuildingClick(map, e));
         map.on("click", "campus-3d", e => searchRoom(map));
-        map.on("click", e => handleBackgroundClick(map, e));
+        // map.on("click", e => handleBackgroundClick(map, e));
     });
 
     currentState.mode = 0;
