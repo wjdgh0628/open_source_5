7c606138dc3be62e69defadc62282b9a7a27c0f3
LazyDev1024 <ironfish1024@gmail.com>
2025-12-27 15:11:08 +0900

    [add] vite 생성

---
+++ portal/src/Login.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react'
+import './Login.css'
+
+function Login() {
+	return (
+		<></>
+	);
+}
+export default Login;
---
+++ portal/src/Portal.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react'
+import './Portal.css'
+
+function Portal() {
+	return (
+		<></>
+	);
+}
+export default Portal;
